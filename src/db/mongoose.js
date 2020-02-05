00cfafb06de1fac34b9e8f55f895006ad390c44b
Daniel Cortes <danncortes@gmail.com>
2020-02-04 21:45:13 -0500

    config production db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,15 @@ const chalk = require('chalk');
 require('dotenv-flow').config();
 const mongoose = require('mongoose');
 
-const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME } = process.env;
-const connectionUrl = `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+const environment = process.env.NODE_ENV;
+
+const { DATABASE_USER, DATABASE_PASS, DATABASE_NAME, DATABASE_HOST } = process.env;
+let connectionUrl = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}/${DATABASE_NAME}?retryWrites=true&w=majority`;
+
+if (environment === 'development') {
+  const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME } = process.env;
+  connectionUrl = `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+}
 
 mongoose.connect(connectionUrl, {
   useNewUrlParser: true,
